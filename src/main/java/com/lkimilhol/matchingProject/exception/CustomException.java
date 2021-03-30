3ef95f2001a12205f0301ef23336f461e1a129da
lkimilhol <lkimilhol@gmail.com>
2021-03-30 22:26:47 +0900

    사용하지 않는 변수 제거

--- src/main/java/com/lkimilhol/matchingProject/exception/CustomException.java
+++ src/main/java/com/lkimilhol/matchingProject/exception/CustomException.java
@@ -1,20 +1,11 @@
 package com.lkimilhol.matchingProject.exception;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-//@RestControllerAdvice
 public class CustomException extends RuntimeException{
     private ErrorInfo errorInfo;
-    private Object[] args;
 
 
     public CustomException(ErrorInfo errorInfo) {
         this.errorInfo = errorInfo;
-    }
-
-    public CustomException(ErrorInfo errorCode, Object... args) {
-        this.errorInfo = errorCode;
-        this.args = args;
     }
 
     public ErrorInfo getErrorInfo() {return errorInfo;}
