226a500d1ca860e13f80daa607a1b5058533c6d9
lkimilhol <lkimilhol@gmail.com>
2021-04-18 16:20:06 +0900

    GenerationType 타입을 mysql ai 사용 되도록 수정

--- src/main/java/com/lkimilhol/matchingProject/domain/Address.java
+++ src/main/java/com/lkimilhol/matchingProject/domain/Address.java
@@ -2,10 +2,7 @@ package com.lkimilhol.matchingProject.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ADDRESS")
@@ -16,6 +13,7 @@ import javax.persistence.Table;
 public class Address {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seq;
 
     private String city;        //시 ex) 서울시, 부산광역시, 광주광역시
--- src/main/java/com/lkimilhol/matchingProject/domain/Member.java
+++ src/main/java/com/lkimilhol/matchingProject/domain/Member.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Member {
     @Id
     @Column(name = "seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nickname;
