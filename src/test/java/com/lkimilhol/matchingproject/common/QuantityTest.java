5c8cb63428a696cd1c9f6cff35d836c1704f7eef
lkimilhol <lkimilhol@gmail.com>
2021-07-28 15:54:12 +0900

    test(QuantityTest): 테스트 추가

--- src/test/java/com/lkimilhol/matchingproject/common/QuantityTest.java
+++ src/test/java/com/lkimilhol/matchingproject/common/QuantityTest.java
@@ -1,9 +1,12 @@
 package com.lkimilhol.matchingproject.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.lkimilhol.matchingproject.exception.NegativeValueException;
 
 class QuantityTest {
 
@@ -15,5 +18,27 @@ class QuantityTest {
         Quantity quantity = new Quantity(7);
         // then
         assertThat(quantity).isNotNull();
+    }
+
+    @DisplayName("차감")
+    @Test
+    void sub() {
+        // given
+        Quantity quantity = new Quantity(10);
+        Quantity subTarget = new Quantity(5);
+        // when
+        Quantity actual = quantity.sub(subTarget);
+        // then
+        assertThat(actual).isEqualTo(new Quantity(5));
+    }
+
+    @DisplayName("생성 실패")
+    @Test
+    void createFailedByNegative() {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> new Quantity(-10))
+                .isInstanceOf(NegativeValueException.class);
     }
 }
