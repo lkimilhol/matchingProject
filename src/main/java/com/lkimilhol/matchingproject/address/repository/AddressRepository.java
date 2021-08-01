48631a7f8e480574824af3dc6ecedac8d8bd1a18
lkimilhol <lkimilhol@gmail.com>
2021-08-01 16:35:19 +0900

    feat(AddressRepository): 주소를 회원으로 조회 가능하도록 추가

--- src/main/java/com/lkimilhol/matchingproject/address/repository/AddressRepository.java
+++ src/main/java/com/lkimilhol/matchingproject/address/repository/AddressRepository.java
@@ -1,8 +1,13 @@
 package com.lkimilhol.matchingproject.address.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lkimilhol.matchingproject.address.domain.Address;
+import com.lkimilhol.matchingproject.member.domain.Member;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findAddressesByMember(Member member);
+
 }
