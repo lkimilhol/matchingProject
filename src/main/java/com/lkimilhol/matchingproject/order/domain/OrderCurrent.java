5f351b053392bfa1e6c47fe4eae10e29cd0b67cf
lkimilhol <lkimilhol@gmail.com>
2021-08-08 16:05:50 +0900

    feat(OrderCurrent,OrderMongoRepositoryTest): 몽고 데이터 생성 테스트

--- src/main/java/com/lkimilhol/matchingproject/order/domain/OrderCurrent.java
+++ src/main/java/com/lkimilhol/matchingproject/order/domain/OrderCurrent.java
@@ -2,11 +2,16 @@ package com.lkimilhol.matchingproject.order.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "domain")
+@Document(collection = "current")
 public class OrderCurrent {
 
     private String name;
     private String job;
+
+    public OrderCurrent(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
 
     @Override
     public String toString() {
