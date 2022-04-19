af44dd16785dd8200360ea374c5b923fd1a5fafd
lkimilhol <lkimilhol@gmail.com>
2022-04-19 16:57:48 +0900

    Country valueOf 메서드 제거

--- src/main/kotlin/com/lkimilhol/matchingproject/member/domain/Country.kt
+++ src/main/kotlin/com/lkimilhol/matchingproject/member/domain/Country.kt
@@ -1,17 +1,6 @@
 package com.lkimilhol.matchingproject.member.domain
 
-import com.lkimilhol.matchingproject.exception.NotFoundCountryException
-
 enum class Country(val country: String) {
     KR("KR"),
     JA("JA");
-
-    @Override
-    fun valueOf():Country {
-        when (name) {
-            "KR" -> return KR
-            "JA" -> return JA
-        }
-        throw NotFoundCountryException()
-    }
 }
